f5cbd4c0af1bbe4e6e76adb7653acb68169ac57f
AltafHusain22 <fiverr.contactme@gmail.com>
2023-05-19 15:43:17 +0600

    megaRoaders products card data loaded from db and added to client

--- src/pages/Home/HomeComponents/CategoryTabs/CategoryTabs.jsx
+++ src/pages/Home/HomeComponents/CategoryTabs/CategoryTabs.jsx
@@ -5,45 +5,67 @@ import Racer from "./Racer/Racer";
 import TurboCharge from "./TurboCharge/TurboCharge";
 import SpeedyMoon from "./SpeedyMoon/SpeedyMoon";
 import ExtreamSpeed from "./ExtreamSpeed/ExtreamSpeed";
+import TurbuBooster from "./TurbuBooster/TurbuBooster";
+import FireStrom from "./FireStrom/FireStrom";
+import MegaRoaders from "./MegaRoaders/MegaRoaders";
 
 const CategoryTabs = () => {
   const [speedies, setSpeedies] = useState([]);
   const [turbos, setTurboThrillers] = useState([]);
   const [SpeedyMoons, setSpeedyMoon] = useState([]);
   const [extreamSpeeds, setExtreamSpeed] = useState([]);
+  const [turboBoosters, setTurboBooster] = useState([]);
+  const [fireStroms, setFireStroms] = useState([]);
+  const [megaRoaders, setMegaRoaders] = useState([]);
 
- 
   useEffect(() => {
+    // speedies
     fetch(`http://localhost:5000/TurboThrillers`)
       .then((res) => res.json())
       .then((data) => {
         setTurboThrillers(data.products);
       });
-
+    // turbos
     fetch(`http://localhost:5000/racerSpeedy`)
       .then((res) => res.json())
       .then((data) => {
         setSpeedies(data.products);
       });
 
-      fetch(`http://localhost:5000/SpeedDemon`)
+    // speedymons
+
+    fetch(`http://localhost:5000/SpeedDemon`)
       .then((res) => res.json())
       .then((data) => {
         setSpeedyMoon(data.products);
       });
-
-      fetch(`http://localhost:5000/extremeSpeedsters`)
+    // extreamspeeds
+    fetch(`http://localhost:5000/extremeSpeedsters`)
       .then((res) => res.json())
       .then((data) => {
         setExtreamSpeed(data.products);
       });
+    // turboBooster
+    fetch(`http://localhost:5000/turboBooster`)
+      .then((res) => res.json())
+      .then((data) => {
+        setTurboBooster(data.products);
+      });
 
+    // firestrom
+    fetch(`http://localhost:5000/fireStroms`)
+      .then((res) => res.json())
+      .then((data) => {
+        setFireStroms(data.products);
+      });
+    // megaOffRoaders
+    fetch(`http://localhost:5000/megaOffRoaders`)
+      .then((res) => res.json())
+      .then((data) => {
+        setMegaRoaders(data.products);
+      });
 
   }, []);
-
-
-
- 
 
   return (
     <section className="container mx-auto px-5 py-2 lg:px-32 lg:pt-12 md:mb-12">
@@ -70,37 +92,28 @@ const CategoryTabs = () => {
             {/* racer speedy tab */}
             <TabPanel>
               <div className="md:mt-10 mt-5 grid md:grid-cols-3 md:gap-10 gap-4">
-                {speedies.map((racers) => <Racer 
-                
-                key={racers._id}
-                racers={racers}
-                
-                >
-                </Racer>)}
+                {speedies.map((racers) => (
+                  <Racer key={racers._id} racers={racers}></Racer>
+                ))}
               </div>
             </TabPanel>
             {/* turbocharge tab */}
             <TabPanel>
               <div className="md:mt-10 mt-5 grid md:grid-cols-3 md:gap-10 gap-4">
-                {turbos.map((turbo) => <TurboCharge 
-                
-                key={turbo._id}
-                turbo={turbo}
-                
-                >
-                </TurboCharge>)}
+                {turbos.map((turbo) => (
+                  <TurboCharge key={turbo._id} turbo={turbo}></TurboCharge>
+                ))}
               </div>
             </TabPanel>
-                  {/* speedyMoon */}
+            {/* speedyMoon */}
             <TabPanel>
               <div className="md:mt-10 mt-5 grid md:grid-cols-3 md:gap-10 gap-4">
-                {SpeedyMoons.map((SpeedyMoonToys) => <SpeedyMoon
-                
-                key={SpeedyMoon._id}
-                SpeedyMoonToys={SpeedyMoonToys}
-                
-                >
-                </SpeedyMoon>)}
+                {SpeedyMoons.map((SpeedyMoonToys) => (
+                  <SpeedyMoon
+                    key={SpeedyMoon._id}
+                    SpeedyMoonToys={SpeedyMoonToys}
+                  ></SpeedyMoon>
+                ))}
               </div>
             </TabPanel>
           </Tabs>
@@ -118,31 +131,37 @@ const CategoryTabs = () => {
             {/* Extreme Speedsters */}
             <TabPanel>
               <div className="md:mt-10 mt-5 grid md:grid-cols-3 md:gap-10 gap-4">
-                {extreamSpeeds.map((extreamSpeed) => <ExtreamSpeed
-                
-                key={extreamSpeed._id}
-                extreamSpeed={extreamSpeed}
-                
-                >
-                </ExtreamSpeed>)}
+                {extreamSpeeds.map((extreamSpeed) => (
+                  <ExtreamSpeed
+                    key={extreamSpeed._id}
+                    extreamSpeed={extreamSpeed}
+                  ></ExtreamSpeed>
+                ))}
               </div>
             </TabPanel>
+
+            {/* Turboo Booster toys */}
             <TabPanel>
-              <p>
-                A kleptomaniacal, lazy, cigar-smoking, heavy-drinking robot who
-                is Fry's best friend. Built in Tijuana, Mexico, he is the Planet
-                Express Ship's cook.
-              </p>
-              <img
-                src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a6/Bender_Rodriguez.png/220px-Bender_Rodriguez.png"
-                alt="Bender Bending Rodriguez"
-              />
+              <div className="md:mt-10 mt-5 grid md:grid-cols-3 md:gap-10 gap-4">
+                {turboBoosters.map((turboBooster) => (
+                  <TurbuBooster
+                    key={turboBooster._id}
+                    turboBooster={turboBooster}
+                  ></TurbuBooster>
+                ))}
+              </div>
             </TabPanel>
+
+            {/* firestrom toys */}
             <TabPanel>
-              <p>
-                Chinese-Martian intern at Planet Express. Fonfon Ru of Kif
-                Kroker.
-              </p>
+              <div className="md:mt-10 mt-5 grid md:grid-cols-3 md:gap-10 gap-4">
+                {fireStroms.map((fireStrom) => (
+                  <FireStrom
+                    key={fireStrom._id}
+                    fireStrom={fireStrom}
+                  ></FireStrom>
+                ))}
+              </div>
             </TabPanel>
           </Tabs>
         </TabPanel>
@@ -157,14 +176,14 @@ const CategoryTabs = () => {
             </TabList>
 
             <TabPanel>
-              <p>
-                Mutant cyclops. Captain of the Planet Express Ship. Love
-                interest of Fry.
-              </p>
-              <img
-                src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d4/Turanga_Leela.png/150px-Turanga_Leela.png"
-                alt="Turanga Leela"
-              />
+               <div className="md:mt-10 mt-5 grid md:grid-cols-3 md:gap-10 gap-4">
+                {megaRoaders.map((megaRoader) => (
+                  <MegaRoaders
+                    key={megaRoader._id}
+                    megaRoader={megaRoader}
+                  ></MegaRoaders>
+                ))}
+              </div>
             </TabPanel>
             <TabPanel>
               <p>
