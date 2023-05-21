cd7b5f2403ee25baa0a6e9b448f0e5b09d91abb1
AltafHusain22 <fiverr.contactme@gmail.com>
2023-05-21 22:35:34 +0600

    readme filed updated

--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -10,7 +10,7 @@ const MyToys = () => {
   useTitle("My Toys");
   const { user } = useContext(AuthContext);
   const [toys, setToys] = useState([]);
-  const [sortingOrder, setSortingOrder] = useState("asc"); // Track the current sorting order
+  const [sortingOrder, setSortingOrder] = useState("asc"); 
 
   useEffect(() => {
     fetch(`https://toys-server-altafhusain22.vercel.app/alltoys`)
